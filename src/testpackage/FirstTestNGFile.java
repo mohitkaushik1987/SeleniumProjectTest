18b586164e9647d1cfa90395bc60239cf1b923cf
mohitkaushik1987 <mohit.d.kaushik@outlook.com>
2016-08-21 16:06:07 -0700

    small changes in test

--- src/testpackage/FirstTestNGFile.java
+++ src/testpackage/FirstTestNGFile.java
@@ -44,7 +44,7 @@ public class FirstTestNGFile {
 	 //driver.findElement(By.linkText("Hello, Sign in")).click();
 	 driver.findElement(By.xpath(".//*[@id='nav-link-yourAccount']/span[1]")).click();
 	 driver.findElement(By.xpath(".//*[@id='ap_email']")).sendKeys("mohit.kaushik.0608@gmail.com");
-	 driver.findElement(By.xpath(".//*[@id='ap_password']")).sendKeys("Villa2018");
+	 driver.findElement(By.xpath(".//*[@id='ap_password']")).sendKeys(“sample password”);
 	 driver.findElement(By.xpath(".//*[@id='signInSubmit']")).click();
 	 String expectedValue_az = "Hello, Mohit";
 	 String actualValue_az = driver.findElement(By.xpath(".//*[@id='nav-link-yourAccount']/span[1]")).getText();
